87a894b8c6b2df1eb7552df0d09170bdaaceab81
bharath <bharathhalemane143@gmail.com>
2025-07-17 17:49:07 +0530

    Detail of Arrays

--- Learning/ArraysL.java
+++ Learning/ArraysL.java
@@ -1,5 +1,6 @@
 package Learning;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ArraysL {
@@ -12,6 +13,56 @@ public class ArraysL {
 
 
         //Declaration of Array
+        //Syntax: dataType[] arrayName;
+
+        int[] arr1; // declaring an array name called arr1
+
+        //Creating and Initializing an Array
+        //Syntax: dataType[] arrayName = { val1, val2, val3,...};
+
+        int[] arr2 = {10, 20, 30, 40, 50, 60, 70, 80, 90};  //declaring, creating and initialized in a single statement
+
+        //Accessing Array Elements
+        //Syntax: arrayName[index];
+
+        int num = arr2[2]; // output: 30
+
+        //Printing an Array
+        //the Arrays class provides a method toString(), which can be used to print array in java.
+        //Syntax: Arrays.toString(arrayName);
+
+//      System.out.println(Arrays.toString(arr2)); //output: [10, 20, 30, 40, 50, 60, 70, 80, 90]
+
+        //Modifying Array Elements
+        //the array elements at any position can be modified but with the same data type.
+
+        arr2[0]=100;
+        arr2[1]=110;
+
+//      System.out.println(Arrays.toString(arr2)); //output: [100, 110, 30, 40, 50, 60, 70, 80, 90]
+
+        //Creating an Array using a " new " Keyword
+        //using this we can create an array of required length and later assign the values.
+        //Syntax: dataType[] arrayName = new dataType[size];
+
+        int[] arr3 = new int[3]; //example
+
+        //Assigning Values
+        arr3[0]=1;
+        arr3[1]=1;
+        arr3[2]=2;
+
+//      System.out.println(Arrays.toString(arr3)); //output: [1, 2, 3]
+
+        //Length of an Array
+        // we can find the array length by using the attribute " length "
+        // arrayName.length
+
+        int lengthOfArr = arr3.length;
+
+//      System.out.println(lengthOfArr); //output: 3
+
+
 
 
 
